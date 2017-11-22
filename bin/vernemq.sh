74054a4caf645785624876454685f83c11a532ed
Mostafa Eltaher <mostafa.mahmoud.eltaher@gmail.com>
2017-11-22 08:03:15 +0000

    Fix config value converted to lower case

--- bin/vernemq.sh
+++ bin/vernemq.sh
@@ -21,7 +21,16 @@ sed -i '/########## Start ##########/,/########## End ##########/d' /etc/vernemq
 
 echo "########## Start ##########" >> /etc/vernemq/vernemq.conf
 
-env | grep DOCKER_VERNEMQ | grep -v 'DISCOVERY_NODE\|DOCKER_VERNEMQ_USER' | cut -c 16- | tr '[:upper:]' '[:lower:]' | sed 's/__/./g' >> /etc/vernemq/vernemq.conf
+format_config() {
+  while read data; do
+    key=$(echo $data | awk -F '=' '{ print $1 }' | tr '[:upper:]' '[:lower:]')
+    value=$(echo $data | awk -F '=' '{ print $2 }')
+    echo "$key = $value"
+  done
+}
+
+#env | grep DOCKER_VERNEMQ | grep -v 'DISCOVERY_NODE\|DOCKER_VERNEMQ_USER' | cut -c 16- | tr '[:upper:]' '[:lower:]' | sed 's/__/./g' >> /etc/vernemq/vernemq.conf
+env | grep DOCKER_VERNEMQ | grep -v 'DISCOVERY_NODE\|DOCKER_VERNEMQ_USER' | cut -c 16- | format_config | sed 's/__/./g' >> /etc/vernemq/vernemq.conf
 
 users_are_set=$(env | grep DOCKER_VERNEMQ_USER)
 if [ ! -z $users_are_set ]
